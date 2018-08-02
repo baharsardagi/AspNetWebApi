01d75440b13325b6a06752003120c441675cb201
Bahar Şardağı <34921851+baharsardagi@users.noreply.github.com>
2018-08-02 11:43:43 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApiDemo.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Age { get; set; }
+    }
+}
